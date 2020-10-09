2faf4c25905e94b3b41e60966d7b3991e2934d91
Michal Zygor <michal@winami.com>
2020-10-09 23:09:29 +0200

    004_exercise_1: Solution for task 1.2

--- src/Iron.java
+++ src/Iron.java
@@ -4,4 +4,18 @@ public class Iron {
     int powerWatts;
     boolean automaticSwitchOff;
 
+    Iron(String manufacturerIn, String modelIn, int powerWattsIn, boolean automaticSwitchOffIn){
+        manufacturer = manufacturerIn;
+        model = modelIn;
+        powerWatts = powerWattsIn;
+        automaticSwitchOff = automaticSwitchOffIn;
+    }
+
+    void PrintResults(int count, Iron iron){
+        System.out.println("Produkt " + count + ":"
+                + " producent: " + iron.manufacturer
+                + ", model: " + iron.model
+                + ", moc: " + iron.powerWatts + "W"
+                + ", automatyczne wyłączanie: "+ iron.automaticSwitchOff);
+    }
 }
--- src/IronsStore.java
+++ src/IronsStore.java
@@ -1,28 +1,9 @@
 public class IronsStore {
     public static void main(String[] args) {
-        Iron iron1 = new Iron();
-        iron1.manufacturer = "Braun";
-        iron1.model = "TextStyle 9";
-        iron1.powerWatts = 2800;
-        iron1.automaticSwitchOff = true;
+        Iron iron1 = new Iron("Braun", "TextStyle 9", 2800,true);
+        Iron iron2 = new Iron("Tefal","Easygliss Plus 2", 2500, false);
 
-        Iron iron2 = new Iron();
-        iron2.manufacturer = "Tefal";
-        iron2.model = "Easygliss Plus 2";
-        iron2.powerWatts = 2500;
-        iron2.automaticSwitchOff = false;
-
-        System.out.println("Produkt 1: "
-                + " producent: " + iron1.manufacturer
-                + ", model: " + iron1.model
-                + ", moc: " + iron1.powerWatts + "W"
-                + ", automatyczne wyłączanie: "+ iron1.automaticSwitchOff);
-
-        System.out.println("Produkt 2: "
-                + " producent: " + iron2.manufacturer
-                + ", model: " + iron2.model
-                + ", moc: " + iron2.powerWatts + "W"
-                + ", automatyczne wyłączanie: "+ iron2.automaticSwitchOff);
-
+        iron1.PrintResults(1, iron1);
+        iron2.PrintResults(2, iron2);
     }
 }
